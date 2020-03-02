b9cb64a186c84839538161e97a2d7fbf6debbd6a
Ksinia <xiatian@mail.ru>
2020-03-02 13:54:22 +0100

    Fix reducer for game updated case

--- src/reducer/games.js
+++ src/reducer/games.js
@@ -1,7 +1,7 @@
 export default function reducer(state = {}, action = {}) {
   switch (action.type) {
     case "GAME_UPDATED": {
-      return { ...state, [action.payload.gameId]: action.payload.currentGame };
+      return { ...state, [action.payload.gameId]: action.payload.game };
     }
     default:
       return state;
