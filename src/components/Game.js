6fcc1d6ffa0663f4b586232cf6d73c0cf80122fd
Ksinia <xiatian@mail.ru>
2020-03-15 20:27:01 +0100

    fix style for game table

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,63 +5,74 @@ function Game(props) {
   return (
     <div>
       {props.game ? (
-        <div key="game" id="game">
-          <Board
-            key="board"
-            clickBoard={props.clickBoard}
-            board={props.board}
-            userBoard={props.userBoard}
-          />
-          <div id="letters" onClick={props.clickLetter}>
-            {props.userLetters.map((letter, index) => {
-              let style = {};
-              if (index === props.chosenLetterIndex) {
-                style = { background: "whitesmoke" };
-              }
-              return (
-                <span
-                  className="letter"
-                  key={index}
-                  data-letter={letter}
-                  data-index={index}
-                  style={style}
-                >
-                  {letter}
-                </span>
-              );
-            })}
+        <div key="game" className="game">
+          <div className="board">
+            <Board
+              key="board"
+              clickBoard={props.clickBoard}
+              board={props.board}
+              userBoard={props.userBoard}
+            />
           </div>
-          {props.user &&
-            props.game.turnOrder[props.game.turn] === props.user.id &&
-            props.game.phase == "turn" && (
-              <button onClick={props.confirmTurn}>Confirm</button>
-            )}
-          {/* next player validates: */}
-          {props.user &&
-            props.game.turnOrder.includes(props.user.id) &&
-            props.user.id ===
-              props.game.turnOrder[props.getNextTurn(props.game)] &&
-            props.game.phase == "validation" && (
-              <button onClick={props.approveTurn}>
-                I confirm{" "}
-                {
-                  props.game.users.find(
-                    user => user.id == props.game.turnOrder[props.game.turn]
-                  ).name
+
+          <div className="controls">
+            <div className="letters" onClick={props.clickLetter}>
+              {props.userLetters.map((letter, index) => {
+                let style = {};
+                if (index === props.chosenLetterIndex) {
+                  style = { background: "whitesmoke" };
                 }
-                's turn
-              </button>
-            )}
-          <p>Game phase: {props.game.phase}</p>
-          <p>
-            {
-              props.game.users.find(
-                user => user.id == props.game.turnOrder[props.game.turn]
-              ).name
-            }
-            's turn
-          </p>
-          <p>Letters left in the bag: {props.game.letters.pot.length}</p>
+                return (
+                  <span
+                    className="letter"
+                    key={index}
+                    data-letter={letter}
+                    data-index={index}
+                    style={style}
+                  >
+                    {letter}
+                  </span>
+                );
+              })}
+            </div>
+            {props.user &&
+              props.game.turnOrder[props.game.turn] === props.user.id &&
+              props.game.phase == "turn" && (
+                <button onClick={props.confirmTurn}>Confirm</button>
+              )}
+            {/* next player validates: */}
+            {props.user &&
+              props.game.turnOrder.includes(props.user.id) &&
+              props.user.id ===
+                props.game.turnOrder[props.getNextTurn(props.game)] &&
+              props.game.phase == "validation" && (
+                <button onClick={props.approveTurn}>
+                  I confirm{" "}
+                  {
+                    props.game.users.find(
+                      user => user.id == props.game.turnOrder[props.game.turn]
+                    ).name
+                  }
+                  's turn
+                </button>
+              )}
+            <p>Game phase: {props.game.phase}</p>
+            {Object.keys(props.game.score).map(key => (
+              <p key={key}>
+                {props.game.users.find(user => user.id == key).name}:{" "}
+                {props.game.score[key]}
+              </p>
+            ))}
+            <p>
+              {
+                props.game.users.find(
+                  user => user.id == props.game.turnOrder[props.game.turn]
+                ).name
+              }
+              's turn
+            </p>
+            <p>Letters left in the bag: {props.game.letters.pot.length}</p>
+          </div>
         </div>
       ) : (
         "Loading Game"
