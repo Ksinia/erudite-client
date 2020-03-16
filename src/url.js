004a9803fb0d6141c5662b0dde8410af9ed69fc6
Ksinia <xiatian@mail.ru>
2020-03-16 02:12:30 +0100

    Change server url to heroku server

--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
-export const url = "http://localhost:4000";
+// export const url = "http://localhost:4000";
 // export const url = "http://192.168.178.17:4000";
+export const url = "https://k-erudite.herokuapp.com";
