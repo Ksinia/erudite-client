2b27b4ce69d2ad450a9aebbd21a2420a9a1a9f3a
Ksinia <xiatian@mail.ru>
2020-03-19 16:20:47 +0100

    Show more info in the room

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 function Room(props) {
+  let waitingFor = 0;
+  if (props.room) {
+    waitingFor = props.room.maxPlayers - props.room.users.length;
+  }
+
   return (
     <div>
       {props.room
         ? [
+            <p key="room-for">Room for {props.room.maxPlayers} players</p>,
             props.room.users.length > 0 ? (
               [
-                <p key="users">Users in this room: </p>,
+                <p key="users">Players already in the room: </p>,
                 props.room.users.map(user => (
                   <div key={user.id}>{user.name}</div>
                 ))
@@ -15,22 +21,27 @@ function Room(props) {
             ) : (
               <p>No users in this room</p>
             ),
+            waitingFor > 0 && (
+              <p key="waiting">Waiting for {waitingFor} more players</p>
+            ),
             !props.user ? (
               <p key="message">Please log in to join the game</p>
             ) : (
-              props.room.phase === "waiting" &&
-              !props.room.users.find(user => user.id === props.user.id) && (
-                <button key="join" name="join" onClick={props.onClick}>
-                  Join the game
-                </button>
-              )
-            ),
-            props.room.users.find(user => user.id === props.user.id) &&
-              props.room.phase === "ready" && (
-                <button key="start" name="start" onClick={props.onClick}>
-                  Start game
-                </button>
-              )
+              [
+                props.room.phase === "waiting" &&
+                  !props.room.users.find(user => user.id === props.user.id) && (
+                    <button key="join" name="join" onClick={props.onClick}>
+                      Join the game
+                    </button>
+                  ),
+                props.room.users.find(user => user.id === props.user.id) &&
+                  props.room.phase === "ready" && (
+                    <button key="start" name="start" onClick={props.onClick}>
+                      Start game
+                    </button>
+                  )
+              ]
+            )
           ]
         : "Loading Room"}
     </div>
