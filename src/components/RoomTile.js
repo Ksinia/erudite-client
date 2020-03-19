26bd082e586907024934447291e75f261ba78c99
Ksinia <xiatian@mail.ru>
2020-03-19 15:31:32 +0100

    Change info shown on the room tile

--- src/components/RoomTile.js
+++ src/components/RoomTile.js
@@ -4,27 +4,22 @@ import "./RoomTile.css";
 
 class RoomTile extends Component {
   render() {
-    const { id, name, maxPlayers, users, phase } = this.props.room;
+    const { id, maxPlayers, users, phase } = this.props.room;
     return (
       <Link to={`/room/${id}`}>
         <div className="room-tile" style={this.props.style}>
           <div className="description">
-            <p className="title">{name}</p>
+            <p className="title">Room {id}</p>
             {phase === "waiting" && <p>Waiting for players</p>}
             {phase === "ready" && <p>Ready for the game</p>}
             {phase === "started" && <p>Game started</p>}
-            {phase === "finished" && <p>Game finished</p>}
-            {/* {maxPlayers > users.length ? (
-              <p>Waiting for players</p>
-            ) : (
-              <p>Ready for the game</p>
-            )} */}
             <p>
-              {users.length} of {maxPlayers} players in the game
+              {phase === "waiting" &&
+                `${users.length} of ${maxPlayers} players in the game`}
             </p>
           </div>
           <div className="players">
-            <p>Current players: </p>
+            <p>Players: </p>
             {users.length > 0 &&
               users.map(user => {
                 return <p key={user.id}>{user.name}</p>;
