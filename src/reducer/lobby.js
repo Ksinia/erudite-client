eed2403629cf1496ed8dd45ffd9eee8ce05711a8
Ksinia <xiatian@mail.ru>
2020-03-19 15:30:40 +0100

    Change way of updating rooms in the store

--- src/reducer/lobby.js
+++ src/reducer/lobby.js
@@ -4,34 +4,11 @@ export default function reducer(state = [], action = {}) {
       return action.payload;
     }
     case "NEW_ROOM": {
-      if (action.payload.oldRoom) {
-        return [...state, action.payload.newRoom].map(room => {
-          if (room.id === action.payload.oldRoom.id) {
-            return action.payload.oldRoom;
-          } else {
-            return room;
-          }
-        });
-      }
-      return [...state, action.payload.newRoom];
+      return [...state, action.payload];
     }
-    case "UPDATED_ROOMS": {
+    case "UPDATED_ROOM": {
       return state.map(room => {
-        if (action.payload.newRoom && room.id === action.payload.newRoom.id) {
-          return action.payload.newRoom;
-        } else if (
-          action.payload.oldRoom &&
-          room.id === action.payload.oldRoom.id
-        ) {
-          return action.payload.oldRoom;
-        } else {
-          return room;
-        }
-      });
-    }
-    case "UPDATE_GAME": {
-      return state.map(room => {
-        if (action.payload.id === room.id) {
+        if (room.id === action.payload.id) {
           return action.payload;
         } else {
           return room;
