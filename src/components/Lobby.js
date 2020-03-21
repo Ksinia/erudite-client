3976604ad91aef276ac30eb960897aed33450d19
Ksinia <xiatian@mail.ru>
2020-03-21 16:56:54 +0100

    Separate rooms by status

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -3,27 +3,59 @@ import RoomTile from "./RoomTile";
 import { colors } from "../colors";
 
 function Lobby(props) {
-  const rooms = props.rooms && (
-    <div className="rooms">
-      {props.rooms.map(room => (
-        <div className="room" key={room.id}>
-          <RoomTile
-            style={{ background: colors[room.id % colors.length] }}
-            room={room}
-          />
-        </div>
-      ))}
-    </div>
-  );
+  const rooms =
+    props.rooms &&
+    props.rooms.reduce(
+      (allRooms, room) => {
+        const roomDiv = (
+          <div className="room" key={room.id}>
+            <RoomTile
+              style={{ background: colors[room.id % colors.length] }}
+              room={room}
+            />
+          </div>
+        );
+        if (
+          props.user &&
+          room.phase === "started" &&
+          room.users.find(user => user.id === props.user.id)
+        ) {
+          allRooms.currentStarted.push(roomDiv);
+        } else if (
+          props.user &&
+          room.phase === "waiting" &&
+          room.users.find(user => user.id === props.user.id)
+        ) {
+          allRooms.currentWaiting.push(roomDiv);
+        } else {
+          allRooms.other.push(roomDiv);
+        }
+        return allRooms;
+      },
+      { currentStarted: [], currentWaiting: [], other: [] }
+    );
+
+  // const rooms = props.rooms && (
+  //   <div className="rooms">
+  //     {props.rooms.map(room => (
+  //       <div className="room" key={room.id}>
+  //         <RoomTile
+  //           style={{ background: colors[room.id % colors.length] }}
+  //           room={room}
+  //         />
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 
   return (
     <div>
-      <p>Please create a new room or enter existing room.</p>
+      <p>Please create a new room or enter an existing room.</p>
       {props.user && (
         <form onSubmit={props.onSubmit}>
           <label htmlFor="maxPlayers">
             {" "}
-            Please specify the quantity of players (from 2 to 4):{" "}
+            Please specify the quantity of players:{" "}
           </label>
           <input
             id="maxPlayers"
@@ -37,7 +69,18 @@ function Lobby(props) {
           <button>Submit</button>
         </form>
       )}
-      <div>{rooms}</div>
+      {rooms.currentStarted.length > 0 && [
+        <p>Your current games:</p>,
+        <div className="rooms">{rooms.currentStarted}</div>
+      ]}
+      {rooms.currentWaiting.length > 0 && [
+        <p>Your rooms</p>,
+        <div className="rooms">{rooms.currentWaiting}</div>
+      ]}
+      {rooms.other.length > 0 && [
+        <p>Other rooms</p>,
+        <div className="rooms">{rooms.other}</div>
+      ]}
     </div>
   );
 }
