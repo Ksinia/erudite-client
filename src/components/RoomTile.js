b344617f9e8a0469f8705e937321c7519a27a9b8
Ksinia <xiatian@mail.ru>
2020-03-22 03:38:50 +0100

    On the room tile show game info, if the game is started

--- src/components/RoomTile.js
+++ src/components/RoomTile.js
@@ -9,7 +9,17 @@ class RoomTile extends Component {
       <Link to={`/room/${id}`}>
         <div className="room-tile" style={this.props.style}>
           <div className="description">
-            <p className="title">Room {id}</p>
+            {phase !== "started" ? (
+              <p className="title">Room {id}</p>
+            ) : (
+              <p className="title">
+                Game{" "}
+                {
+                  this.props.room.games.find(game => game.phase !== "finished")
+                    .id
+                }
+              </p>
+            )}
             {phase === "waiting" && <p>Waiting for players</p>}
             {phase === "ready" && <p>Ready for the game</p>}
             {phase === "started" && <p>Game started</p>}
@@ -20,10 +30,13 @@ class RoomTile extends Component {
           </div>
           <div className="players">
             <p>Players: </p>
-            {users.length > 0 &&
-              users.map(user => {
-                return <p key={user.id}>{user.name}</p>;
-              })}
+            {users.length > 0 && phase === "started"
+              ? this.props.room.games
+                  .find(game => game.phase !== "finished")
+                  .turnOrder.map(id => (
+                    <p key={id}>{users.find(user => user.id === id).name}</p>
+                  ))
+              : users.map(user => <p key={user.id}>{user.name}</p>)}
           </div>
         </div>
       </Link>
