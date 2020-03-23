7b1460713a0e6d23696c9674a6f5672f06653106
Ksinia <xiatian@mail.ru>
2020-03-23 15:25:48 +0100

    Fix url development backend url

--- src/url.js
+++ src/url.js
@@ -1,3 +1,5 @@
-// export const url = "http://localhost:4000";
-// export const url = "http://192.168.178.17:4000";
-export const url = "https://k-erudite.herokuapp.com";
+let url = "https://k-erudite.herokuapp.com";
+if (process.env.NODE_ENV === "development") {
+  url = "http://localhost:4000";
+}
+export { url };
