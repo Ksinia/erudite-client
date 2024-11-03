7f0800899c493e59ead21da021cbe707a3f13ba0
Ksinia <xiatian@mail.ru>
2024-11-03 22:38:20 +0100

    Rewrite login without material ui

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,36 +1,8 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
+import styles from './LogIn.module.css';
 
 import TranslationContainer from './Translation/TranslationContainer';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
 type OwnProps = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -39,71 +11,53 @@ type OwnProps = {
   error: string | null;
 };
 
-export default function SignIn(props: OwnProps) {
-  const classes = useStyles();
-
+export default function LogIn(props: OwnProps) {
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          <TranslationContainer translationKey="log_in" />
-        </Typography>
-        <form className={classes.form} onSubmit={props.onSubmit} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <TranslationContainer translationKey="log_in" />
+      </h1>
+      <form onSubmit={props.onSubmit} noValidate>
+        <label>
+          <TranslationContainer translationKey="name" />
+          <input
+            type="text"
             id="name"
-            label={<TranslationContainer translationKey="name" />}
             name="name"
+            required
             autoComplete="username"
             autoFocus
             onChange={props.onChange}
             value={props.values.name}
+            className={styles.input}
           />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label={<TranslationContainer translationKey="password" />}
+        </label>
+        <label>
+          <TranslationContainer translationKey="password" />
+          <input
             type="password"
             id="password"
+            name="password"
+            required
             autoComplete="current-password"
             onChange={props.onChange}
             value={props.values.password}
+            className={styles.input}
           />
-          {/*render error messages from server*/}
-          <p style={{ color: 'red' }}>{props.error}</p>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            <TranslationContainer translationKey="log_in" />
-          </Button>
-          <Grid container justifyContent="center">
-            <Grid item>
-              <Link to="/signup">
-                <TranslationContainer translationKey="no_account" />
-              </Link>
-              <Grid item>
-                <Link to="/forgot-password">
-                  <TranslationContainer translationKey="forgot" />
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+        </label>
+        <p className={styles.errorMessage}>{props.error}</p>
+        <button type="submit" className={styles.submitButton}>
+          <TranslationContainer translationKey="log_in" />
+        </button>
+        <div className={styles.links}>
+          <Link to="/signup">
+            <TranslationContainer translationKey="no_account" />
+          </Link>
+          <Link to="/forgot-password">
+            <TranslationContainer translationKey="forgot" />
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
